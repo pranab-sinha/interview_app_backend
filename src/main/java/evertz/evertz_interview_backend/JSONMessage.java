4c9cabadaad19f6bf807661e787e2a2b3d45ed76
EVERTZ_MICROSYS\psinha <psinha@evertz.com>
2019-09-17 18:08:34 -0400

    Adding api for details

--- src/main/java/evertz/evertz_interview_backend/JSONMessage.java
+++ src/main/java/evertz/evertz_interview_backend/JSONMessage.java
@@ -15,6 +15,7 @@ public class JSONMessage {
 
 	public static String JSONParse(String JSONRequestMessage) throws org.json.simple.parser.ParseException {
 		//JSON parser object to parse read the response
+		String result = "";
 		if(JSONRequestMessage == null) {
 			return "NULL MESSAGE RECEIVED";
 		}
@@ -34,61 +35,23 @@ public class JSONMessage {
 				JSONObject studentObject = (JSONObject) slide.get("EvertzInterviewApp");
 				System.out.println(studentObject);
 
-				String website = (String) studentObject.get("Subsystem");   
-				System.out.println(website);
-				String firstName = (String) studentObject.get("Command");   
-				System.out.println(firstName);
-				JSONArray parameterList = (JSONArray) studentObject.get("ParameterList");
-				JSONObject job = (JSONObject) parameterList.get(0);
-				System.out.println("New ab - " +job);
-		
-				String registerNumber = (String)job.get("RegisterNumber");
-				String studentName = (String)job.get("StudentName");
-				String dob = (String)job.get("DOB");
-				String college = (String)job.get("College");
-				String gender = (String)job.get("Gender");
-				String degree = (String)job.get("Degree");
-				String stream = (String)job.get("Stream");
-				String yearofpassing = (String)job.get("YearOfPassing");
-				String cgpa = (String)job.get("CGPA");
-				String email = (String)job.get("Email");
-				String Mobile = (String)job.get("Mobile");
-				String LanguageSelected = (String)job.get("LanguageSelected");
-				System.out.println("Testing ---");
-				String Gender = "";
-				if(gender == "Male") {
-					Gender = "b'1'";
-				}
-				else {
-					Gender = "b'0'";
-				}
-				System.out.println(Gender);
+				String subsystem = (String) studentObject.get("Subsystem");   
+				System.out.println(subsystem);
 				
-				DateFormat formatter = new SimpleDateFormat("MM-DD-yyyy"); 
-				Date date = (Date)formatter.parse(dob);
-				SimpleDateFormat newFormat = new SimpleDateFormat("yyyy-MM-dd");
-				String dateOfBirth = newFormat.format(date).toString();
+				String command = (String) studentObject.get("Command");   
+				System.out.println(command);
 				
-				String updateSQL="INSERT INTO evertz_interview_app.candidate_details ( `REG_NO`, `NAME`, `DOB`, `GENDER`, `EMAIL`, `MOB_NO`, `COLLEGE_DETAILS_ID`, `DEGREE_ID`, `BRANCH_ID`, `GRAD_YEAR`, `CGPA`) VALUES ( '" + registerNumber + "', '" + studentName+ "', '" + dateOfBirth + "', " + Gender + ", '"+ email + "', '"+ Mobile + "', '1', '1', '1', '"+ yearofpassing +"', '"+cgpa +"');";
-
-
-				System.out.println(updateSQL);
-				DAOLayer.updateData(updateSQL);
-
-
-
-
+				if((subsystem.equals("StudentRegistration")) && (command.equals("Save"))) {
+					result = StudentRegistration.Register(message);
+				}
+				if((subsystem.equals("UI")) && (command.equals("Get"))) {
+					result = UIDropDownList.getListJSON(message);
+				}
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return "{\r\n" + 
-				"	\"EvertzInterviewApp\" : {\r\n" + 
-				"		\"Subsystem\" : \"StudentRegistration\",\r\n" + 
-				"		\"Command\" : \"Status\",\r\n" + 
-				"		\"Registartion\" : \"Success\"\r\n" + 
-				"	}\r\n" + 
-				"}";	
+		return result;	
 	}
 }
