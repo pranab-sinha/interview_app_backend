7c7881d4592b8552a41e864eb1359ace7ce45728
EVERTZ_MICROSYS\psinha <psinha@evertz.com>
2019-09-18 07:03:34 -0400

    Adding proper print statement

--- src/main/java/evertz/evertz_interview_backend/UIDropDownList.java
+++ src/main/java/evertz/evertz_interview_backend/UIDropDownList.java
@@ -75,6 +75,7 @@ public class UIDropDownList {
 			    	jsonoutput.add(jsonFinal);
 		        
 			    	result = jsonoutput.toString(); 
+			    	System.out.println("Result JSON - " + result);
 			    	
 				}
 				catch (Exception e) {
